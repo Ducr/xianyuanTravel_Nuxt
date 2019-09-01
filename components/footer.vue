d958ca40e9b919acca7f96921d44dc99394f9441
Ducr <Dulant_du@126.com>
2019-09-01 22:07:25 +0800

    02.闲云旅游网公共部分（页头、页尾）样式设置完成

--- components/footer.vue
+++ components/footer.vue
@@ -1,6 +1,44 @@
 <template>
-  <div class="footer">
-      页尾
+  <div class="container">
+      <div class="footer">
+            <el-row type="flex"  class="support" justify="space-between">
+                <div class="travel">
+                    <h5>闲云旅游旅游网</h5>
+                    <p>上亿旅行者共同打造的"旅行神器"</p>
+                    <p><span>60,000</span>&nbsp;多个全球旅游目的地</p>
+                    <p><span>60,000</span>&nbsp;多个细分目的地新玩法</p>
+                    <p><span>760,000,000</span>&nbsp;次攻略下载</p>
+                    <p><span>38,000</span>&nbsp;家旅游产品供应商</p>
+                </div>
+                <div class="introduction">
+                    <h5>关于我们</h5>
+                    <p>隐私政策&nbsp;商标声明</p>
+                    <p>服务协议&nbsp;游记协议</p>
+                    <p>商城平台服务协议</p>
+                    <p>网络信息侵权通知指引</p>
+                    <p>闲云旅游旅游网服务监督员</p>
+                    <p>网站地图加入闲云旅游</p>
+                </div>
+                <div class="service">
+                    <h5>旅行服务</h5>
+                    <p>旅游攻略&nbsp;酒店预订</p>
+                    <p>旅游特价&nbsp;国际租车</p>
+                    <p>旅游问答&nbsp;旅游保险</p>
+                    <p>旅游指南&nbsp;订火车票</p>
+                    <p>旅游资讯&nbsp;APP下载</p>
+                </div>
+                <div class="QRCode">
+                    <div class="picture">
+                        <img src="./../static/images/QRCode.png" alt="">
+                    </div>
+                    <p>关注我们</p>
+                </div>
+            </el-row>
+            <div class="copyright">
+                <p>京ICP备08001421号 京公网安备110108007702 Copyright © 2016-2019 博学谷 All Rights Reserved</p>
+            </div>
+      </div>
+      <!-- <div class="box" style="height:500px;background-color:#323232"></div> -->
   </div>
 </template>
 
@@ -10,6 +48,66 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+    .container{
+    background-color: #323232;
+    color:#ccc;
+    font-size: 12px;
+    .footer{
+        margin: 0 auto;
+        width: 1000px;
+        .support{
+            padding-top: 30px;
+            .travel,.introduction,.service{
+                width:250px;
+                h5{
+                    font-size: 16px;
+                    height: 21px;
+                    line-height: 21px;
+                    margin-bottom:10px;
+                }
+                p{
+                    height: 21px;
+                    line-height: 21px;
+                    span{
+                        color:#FFA500;
+                    }
+                }
+            }
+            .introduction,.service{
+                width:208px;
+            }
+            .QRCode{
+                width:250px;
+                height:162px;
+                    .picture{
+                        width:250px;
+                        height: 146px;
+                        img{
+                            display: block;
+                            margin:0 auto;
+                            width:140px;
+                            height: 140px;
+                        }
+                    }
+                    p{
+                        text-align: center;
+                    }
+            
+            }
 
+        }
+        .copyright{
+            margin: 0 auto;
+            width: 1000px;
+            margin-top:20px;
+            border-top:1px #656565 solid;
+
+            p{
+                padding:50px 0;
+                text-align: center;
+            }
+        }
+    }
+}
 </style>
--- components/header.vue
+++ components/header.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="header">
-      <div class="logo">
-          <img src="./../static/logo.jpg" alt="">
-      </div>
-      <div class="navs">
-          <nuxt-link to='/'>首页</nuxt-link>
-          <nuxt-link to='/post'>旅游攻略</nuxt-link>
-          <nuxt-link to='/hotel'>酒店</nuxt-link>
-          <nuxt-link  to='/air'>国内机票</nuxt-link>
-      </div>
-      <div class="login">
-          <nuxt-link  to='/user/login'>登录 / 注册</nuxt-link>
-      </div>
+    <el-row class="tab" type="flex" justify="space-between">
+        <div class="logo">
+            <!-- <nuxt-link> 相当于vue里面的<router-link> -->
+            <nuxt-link to='/'><img src="./../static/images/logo.jpg" alt=""></nuxt-link>
+        </div>
+        <el-row type="flex" class="navs">
+            <nuxt-link to='/'>首页</nuxt-link>
+            <nuxt-link to='/post'>旅游攻略</nuxt-link>
+            <nuxt-link to='/hotel'>酒店</nuxt-link>
+            <nuxt-link  to='/air'>国内机票</nuxt-link>
+        </el-row>
+        <div class="login">
+            <nuxt-link  to='/user/login'>登录 / 注册</nuxt-link>
+        </div>
+    </el-row>
   </div>
 </template>
 
@@ -21,6 +24,48 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.header{
+    width:100%;
+    height: 60px;
+    line-height: 60px;
+    box-shadow:0 2px 2px #ddd; 
+    border-bottom:1px #ddd solid;
+}
+.tab{
+    width:1000px;
+    margin:0 auto;
+}
+.logo{
+    padding-top:9px;
+    img{
+    display: block;
+    width:156px;
+    height: 42px;
+    }
+}
+.navs{
+    flex:1;
+    margin:0 20px;
+    a{
+        display: block;
+        box-sizing: border-box;
+        padding: 0 20px;
+        height: 60px;
+        &:hover{
+            color: #429efd;
+            border-bottom:5px #429efd solid;
+        }
+    }
+    // 该class是nuxt自动匹配nuxt-link的to的值，如果url和to的值相等就会自动加上下面的class
+    .nuxt-link-exact-active{
+        background-color: #429efd;
+        color: #fff;
+        &:hover{
+            color:#fff;
+        }
+    }
+}
+</style>>
 
 </style>
